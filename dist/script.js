55f472ce8cfe6cb859b0572193def9e6b3d91744
Malsim Dauhanosav <max_air@bk.ru>
2022-03-12 20:34:28 +0300

    Разбор прототипов

--- dist/script.js
+++ dist/script.js
@@ -93,31 +93,36 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-(() => {
-  const $ = function (selector) {
-    const elements = document.querySelectorAll(selector);
-    console.log(elements);
-    const obj = {};
-
-    obj.hide = () => {
-      elements.forEach(element => {
-        element.style.display = 'none';
-      });
-      return obj;
-    };
-
-    obj.show = () => {
-      elements.forEach(element => {
-        element.style.display = '';
-      });
-      return obj;
-    };
-
-    return obj;
-  };
-
-  window.$ = $;
-})();
+// (() => {
+//     const $ = function (selector) {
+//         const elements = document.querySelectorAll(selector);
+//         console.log(elements);
+//         const obj = {};
+//         obj.hide = () => {
+//             elements.forEach(element => {
+//                 element.style.display = 'none';
+//             });
+//             return obj;
+//         };
+//         obj.show = () => {
+//             elements.forEach(element => {
+//                 element.style.display = '';
+//             });
+//             return obj;
+//         };
+//         return obj;
+//     };
+//     window.$ = $;
+// })();
+const person = {
+  name: "Max",
+  age: 31,
+  sex: "famele",
+  say: function () {
+    console.log("hello");
+  }
+};
+let person2 = new person.prototype.say(); //console.log(new person.prototype);
 
 /***/ }),
 
--- src/js/lib/core.js
+++ src/js/lib/core.js
@@ -1,23 +1,38 @@
-(() => {
-    const $ = function (selector) {
-        const elements = document.querySelectorAll(selector);
-        console.log(elements);
-        const obj = {};
-        obj.hide = () => {
-            elements.forEach(element => {
-                element.style.display = 'none';
-                
-            });
-            return obj;
-        };
-        obj.show = () => {
-            elements.forEach(element => {
-                element.style.display = '';
-                
-            });
-            return obj;
-        };
-        return obj;
-    };
-    window.$ = $;
-})();
+// (() => {
+//     const $ = function (selector) {
+//         const elements = document.querySelectorAll(selector);
+//         console.log(elements);
+//         const obj = {};
+//         obj.hide = () => {
+//             elements.forEach(element => {
+//                 element.style.display = 'none';
+
+//             });
+//             return obj;
+//         };
+//         obj.show = () => {
+//             elements.forEach(element => {
+//                 element.style.display = '';
+
+//             });
+//             return obj;
+//         };
+//         return obj;
+//     };
+//     window.$ = $;
+// })();
+
+const person = {
+    name: "Max",
+    age: 31,
+    sex: "famele",
+    say: function(){
+        console.log("hello");
+    }
+};
+
+
+let person2 = new person.prototype.say();
+
+//console.log(new person.prototype);
+
